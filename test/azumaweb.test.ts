ed904c4cf6daee886f339f96eca2f464e146a962
Ratheesh Nair <ratheesh.nair@netwitness.com>
2024-03-14 23:42:45 +0530

    Initial commit

---
+++ test/azumaweb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Azumaweb from '../lib/azumaweb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/azumaweb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Azumaweb.AzumawebStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
